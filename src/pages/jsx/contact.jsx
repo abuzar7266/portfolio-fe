29861cf197526f537a6bdece6e12e20b897c6daa
Abuzar <i190531@nu.edu.pk>
2021-09-03 18:36:56 -0700

    Home section about alignment

--- src/pages/jsx/contact.jsx
+++ src/pages/jsx/contact.jsx
@@ -1,15 +1,108 @@
-import React from 'react';
-import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from 'react';
+import { Container,Row,Col } from 'react-bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { Component } from 'react';
-
-
+import { Card,Dropdown,Form,DropdownButton } from 'react-bootstrap';
+import imgs from '../../images/myPic.jpg';
+import { Image } from 'react-bootstrap'; 
 class Contact extends Component
 {
   render()
   {
     return (<>
-        <h1>Soon will be available</h1>
+       <Container fluid>
+            <Row>
+              <br /><br /><br />
+            </Row>
+            <Row>
+              <Col xs={0}
+                   sm={0}
+                   md={0}
+                   lg={7}>
+                    <Col xs={0}
+                        sm={0}
+                        md={0}
+                        lg={7}>
+                          <Card style={{boxShadow:"5px 5px 10px black",marginBottom:"20px",maxWidth:"300px"}}>
+                       <Card.Header style={{backgroundColor:"#161C27",height:"40px",color:"white"}}>
+                       Contact us
+                       </Card.Header>
+                          <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
+                              <Card.Text>
+                              <Image height="110px" width="110px" style={{borderRadius:"50%",paddingTop:"20px"}} src={imgs} fluid/>
+                              <p style={{fontSize:"15px",paddingTop:"10px"}}>@Abuzar</p>
+                              </Card.Text>
+                          </Card.Body>
+                        </Card>
+                    </Col>
+              </Col>
+              <Col xs={12}
+                   sm={12}
+                   md={12}
+                   lg={4}
+                   >
+                     <Card style={{boxShadow:"5px 5px 10px black",maxWidth:"700px"}}>
+                       <Card.Header style={{backgroundColor:"#161C27",height:"45px",color:"white"}}>
+                       <div className="row">
+                       <div class="col-12">
+                                Send Message
+                       </div>
+                       </div>
+                       </Card.Header>
+                       <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
+                           <Card.Text>
+                           <form>
+                        <div class="form-group row" style={{marginBottom:"10px"}}>
+                            <div class="col-12">
+                                <Form.Control type="text" placeholder="Subject" />
+                            </div>
+                        </div>
+                        <div class="form-group row" style={{marginBottom:"10px"}}>
+                            <div class="col-6">
+                                <Form.Control type="text" placeholder="First Name" />
+                            </div>
+                            <div class="col-6">
+                                <Form.Control type="text" placeholder="Last Name" />
+                            </div>
+                        </div>
+                        <div class="form-group row" style={{marginBottom:"10px"}}>
+                            <div class="col-7">
+                                <label class="sr-only" for="exampleInputEmail3">Email address</label>
+                                <Form.Control type="email" placeholder="Email Address" />
+                            </div>
+                            <div class="col-5">
+                                <Form.Control type="number" placeholder="Mobile No." />
+                            </div>
+                        </div>
+                        <div class="form-group row" style={{marginBottom:"10px"}}>
+                            <div class="col-8">
+                            <label class="sr-only" for="attachImages">Attachments</label>
+                                <input
+                                    type="file"
+                                    id="attachImages"
+                                    aria-describedby="attachImagesAddon01"
+                                    multiple/>
+                            </div>
+                        </div>
+                        <div class="form-group row" style={{marginBottom:"10px"}}>
+                            <div class="col-12">
+                                    <textarea
+                                    className="form-control"
+                                    id="exampleFormControlTextarea1"
+                                    rows="5"
+                                    placeholder="Write something..."
+                                    />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <button type="submit" class="btn btn-primary btn-sm ml-1" style={{margin:"10px",boxShadow:"1px 1px 10px black"}}> <i className="fa fa-send"></i> Send  </button>
+                        </div>
+                    </form>
+                          </Card.Text>
+                       </Card.Body>
+                   </Card>
+              </Col>
+            </Row>
+       </Container>
     </>);
   }
 };
