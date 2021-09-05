6873d553f7defe7e18f0000953e012578075a412
Abuzar <i190531@nu.edu.pk>
2021-09-05 11:17:19 -0700

    Show Post Full Added-Initial

--- src/pages/jsx/about.jsx
+++ src/pages/jsx/about.jsx
@@ -54,18 +54,21 @@ class About extends Component
                     xs={10}
                     sm={10}
                     md={4}
-                    lg={3}
+                    lg={2}
                     style={{textAlign:'left',overflow:"hidden"}}
                     className="align-self-center"
                 >
                 <Card style={{boxShadow:"5px 5px 10px black",marginBottom:"20px"}}>
                        <Card.Header style={{backgroundColor:"#161C27",height:"40px",color:"white"}}>
-                           Current Job Status
+                            <i className="fa fa-info"></i> Information
                        </Card.Header>
                        <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
-                           <Card.Text>
-                           <p style={{fontSize:"12px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget auctor eros, at dapibus orci. Aliquam ornare odio ac magna porttitor, eu tincidunt mi accumsan. Praesent fringilla massa pellentesque elit fermentum tincidunt. Aenean non justo et neque vulputate lobortis quis vel nunc. Nullam a blandit sapien, sed rhoncus odio. Vestibulum vel ex dictum, dictum nisi at, iaculis sapien. In lacinia fermentum imperdiet.
-Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla sollicitudin mauris in est feugiat porta. In nec nisi semper, bibendum sem quis, ornare dolor. Nulla a erat eros. Ut at orci varius, pretium mauris ac, ultricies dui</p>
+                           <Card.Text style={{marginLeft:"10px"}}>
+                           <p style={{fontSize:"12px"}}><b>Name</b>: Abuzar</p>
+                           <p style={{fontSize:"12px"}}><b>Degree Pursuing </b>: BS(CS)</p>
+                           <p style={{fontSize:"12px"}}><b>Country </b>: Pakistan</p>
+                           <p style={{fontSize:"12px"}}><b>Home City </b>: Gujrawala</p>
+                           <p style={{fontSize:"12px"}}><b>Current City</b>: Islamabad</p>
                           </Card.Text>
                        </Card.Body>
                    </Card>
@@ -74,18 +77,18 @@ Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla s
                     xs={10}
                     sm={10}
                     md={4}
-                    lg={3}
+                    lg={2}
                     style={{textAlign:'left',overflow:"hidden"}}
                     className="align-self-center"
                 >
                 <Card style={{boxShadow:"5px 5px 10px black",marginBottom:"20px"}}>
                        <Card.Header style={{backgroundColor:"#161C27",height:"40px",color:"white"}}>
-                           Current Job Status
+                           Work Experience
                        </Card.Header>
                        <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
                            <Card.Text>
-                           <p style={{fontSize:"12px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget auctor eros, at dapibus orci. Aliquam ornare odio ac magna porttitor, eu tincidunt mi accumsan. Praesent fringilla massa pellentesque elit fermentum tincidunt. Aenean non justo et neque vulputate lobortis quis vel nunc. Nullam a blandit sapien, sed rhoncus odio. Vestibulum vel ex dictum, dictum nisi at, iaculis sapien. In lacinia fermentum imperdiet.
-Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla sollicitudin mauris in est feugiat porta. In nec nisi semper, bibendum sem quis, ornare dolor. Nulla a erat eros. Ut at orci varius, pretium mauris ac, ultricies dui</p>
+                           <p style={{fontSize:"12px"}}>Teaching Assistant at FAST NUCES <p style={{fontSize:"10px",color:"white",opacity:"80%"}}>Islamabad, Pakistan <p style={{fontSize:"7px",color:"white",opacity:"80%"}}>Sep 2021 - Present</p> </p> </p>
+                           <p style={{fontSize:"12px"}}>Officer Buddy at National University Computing Society <p style={{fontSize:"8px",color:"white",opacity:"80%"}}> FAST NUCES <p style={{fontSize:"10px",color:"white",opacity:"80%"}}>Islamabad, Pakistan <p style={{fontSize:"7px",color:"white",opacity:"80%"}}>Sep 2021 - Present</p> </p> </p> </p>
                           </Card.Text>
                        </Card.Body>
                 </Card>
@@ -107,12 +110,11 @@ Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla s
                 >
                 <Card style={{boxShadow:"5px 5px 10px black",marginBottom:"20px"}}>
                        <Card.Header style={{backgroundColor:"#161C27",height:"40px",color:"white"}}>
-                           Current Job Status
+                           Education
                        </Card.Header>
                        <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
                            <Card.Text>
-                           <p style={{fontSize:"12px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget auctor eros, at dapibus orci. Aliquam ornare odio ac magna porttitor, eu tincidunt mi accumsan. Praesent fringilla massa pellentesque elit fermentum tincidunt. Aenean non justo et neque vulputate lobortis quis vel nunc. Nullam a blandit sapien, sed rhoncus odio. Vestibulum vel ex dictum, dictum nisi at, iaculis sapien. In lacinia fermentum imperdiet.
-Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla sollicitudin mauris in est feugiat porta. In nec nisi semper, bibendum sem quis, ornare dolor. Nulla a erat eros. Ut at orci varius, pretium mauris ac, ultricies dui</p>
+                           <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Python & ML-Libs<p style={{fontSize:"12px"}}>  <i className="fa fa-certificate"></i>  C++ <p style={{fontSize:"12px"}}>  <i className="fa fa-certificate"></i>  R-Lang <p style={{fontSize:"12px"}}>  <i className="fa fa-certificate"></i>  React.js and React Native <p style={{fontSize:"12px"}}>  <i className="fa fa-certificate"></i> Bootstrap 5 </p> </p></p></p></p>
                           </Card.Text>
                        </Card.Body>
                    </Card>
@@ -121,18 +123,18 @@ Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla s
                     xs={10}
                     sm={10}
                     md={4}
-                    lg={3}
+                    lg={2}
                     style={{textAlign:'left',overflow:"hidden"}}
                     className="align-self-center"
                 >
                     <Card style={{boxShadow:"5px 5px 10px black",marginBottom:"20px"}}>
                        <Card.Header style={{backgroundColor:"#161C27",height:"40px",color:"white"}}>
-                           Current Job Status
+                           Education
                        </Card.Header>
                        <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
                            <Card.Text>
-                           <p style={{fontSize:"12px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget auctor eros, at dapibus orci. Aliquam ornare odio ac magna porttitor, eu tincidunt mi accumsan. Praesent fringilla massa pellentesque elit fermentum tincidunt. Aenean non justo et neque vulputate lobortis quis vel nunc. Nullam a blandit sapien, sed rhoncus odio. Vestibulum vel ex dictum, dictum nisi at, iaculis sapien. In lacinia fermentum imperdiet.
-Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla sollicitudin mauris in est feugiat porta. In nec nisi semper, bibendum sem quis, ornare dolor. Nulla a erat eros. Ut at orci varius, pretium mauris ac, ultricies dui</p>
+                           <p style={{fontSize:"12px"}}>Bachelors in Computer Science<p style={{fontSize:"10px",color:"white",opacity:"80%"}}> FAST NUCES <p style={{fontSize:"8px",color:"white",opacity:"80%"}}>Islamabad, Pakistan <p style={{fontSize:"7px",color:"white",opacity:"80%"}}>Sep 2021 - Present</p> </p> </p> </p>
+                           <p style={{fontSize:"12px"}}>Intermediate in Computer Science (I.C.S)<p style={{fontSize:"10px",color:"white",opacity:"80%"}}> Punjab Group Of Colleges<p style={{fontSize:"8px",color:"white",opacity:"80%"}}>Gujranwala, Pakistan <p style={{fontSize:"7px",color:"white",opacity:"80%"}}>Sep 2021 - Present</p> </p> </p> </p>
                           </Card.Text>
                        </Card.Body>
                    </Card>
@@ -141,18 +143,17 @@ Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla s
                     xs={10}
                     sm={10}
                     md={4}
-                    lg={3}
+                    lg={2}
                     style={{textAlign:'left',overflow:"hidden"}}
                     className="align-self-center"
                 >
                 <Card style={{boxShadow:"5px 5px 10px black",marginBottom:"20px"}}>
                        <Card.Header style={{backgroundColor:"#161C27",height:"40px",color:"white"}}>
-                           Current Job Status
+                           Soft Skills
                        </Card.Header>
                        <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
                            <Card.Text>
-                           <p style={{fontSize:"12px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget auctor eros, at dapibus orci. Aliquam ornare odio ac magna porttitor, eu tincidunt mi accumsan. Praesent fringilla massa pellentesque elit fermentum tincidunt. Aenean non justo et neque vulputate lobortis quis vel nunc. Nullam a blandit sapien, sed rhoncus odio. Vestibulum vel ex dictum, dictum nisi at, iaculis sapien. In lacinia fermentum imperdiet.
-Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla sollicitudin mauris in est feugiat porta. In nec nisi semper, bibendum sem quis, ornare dolor. Nulla a erat eros. Ut at orci varius, pretium mauris ac, ultricies dui</p>
+                           <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Effective Communication <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Team Work <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Problem-Solving </p></p></p>
                           </Card.Text>
                        </Card.Body>
                    </Card>
@@ -176,18 +177,17 @@ Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla s
                     xs={10}
                     sm={10}
                     md={4}
-                    lg={3}
+                    lg={2}
                     style={{textAlign:'left',overflow:"hidden"}}
                     className="align-self-center"
                 >
                 <Card style={{boxShadow:"5px 5px 10px black",marginBottom:"20px"}}>
                        <Card.Header style={{backgroundColor:"#161C27",height:"40px",color:"white"}}>
-                           Current Job Status
+                           Hobbies
                        </Card.Header>
                        <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
                            <Card.Text>
-                           <p style={{fontSize:"12px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget auctor eros, at dapibus orci. Aliquam ornare odio ac magna porttitor, eu tincidunt mi accumsan. Praesent fringilla massa pellentesque elit fermentum tincidunt. Aenean non justo et neque vulputate lobortis quis vel nunc. Nullam a blandit sapien, sed rhoncus odio. Vestibulum vel ex dictum, dictum nisi at, iaculis sapien. In lacinia fermentum imperdiet.
-Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla sollicitudin mauris in est feugiat porta. In nec nisi semper, bibendum sem quis, ornare dolor. Nulla a erat eros. Ut at orci varius, pretium mauris ac, ultricies dui</p>
+                           <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Writing Programs <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Travelling <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Gaming <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Graphics Designing </p> </p> </p> </p>
                           </Card.Text>
                        </Card.Body>
                    </Card>
@@ -196,18 +196,17 @@ Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla s
                     xs={10}
                     sm={10}
                     md={4}
-                    lg={3}
+                    lg={2}
                     style={{textAlign:'left',overflow:"hidden"}}
                     className="align-self-center"
                 >
                     <Card style={{boxShadow:"5px 5px 10px black",marginBottom:"20px"}}>
                        <Card.Header style={{backgroundColor:"#161C27",height:"40px",color:"white"}}>
-                           Current Job Status
+                           Areas of Expertise
                        </Card.Header>
                        <Card.Body style={{backgroundColor:"#1C2331",color:"white"}}>
                            <Card.Text>
-                           <p style={{fontSize:"12px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget auctor eros, at dapibus orci. Aliquam ornare odio ac magna porttitor, eu tincidunt mi accumsan. Praesent fringilla massa pellentesque elit fermentum tincidunt. Aenean non justo et neque vulputate lobortis quis vel nunc. Nullam a blandit sapien, sed rhoncus odio. Vestibulum vel ex dictum, dictum nisi at, iaculis sapien. In lacinia fermentum imperdiet.
-Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla sollicitudin mauris in est feugiat porta. In nec nisi semper, bibendum sem quis, ornare dolor. Nulla a erat eros. Ut at orci varius, pretium mauris ac, ultricies dui</p>
+                           <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Machine Learning /Deep Learning <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Web-Development <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Graphics Designer <p style={{fontSize:"12px"}}> <i className="fa fa-certificate"></i> Game Development </p> </p> </p> </p>
                           </Card.Text>
                        </Card.Body>
                    </Card>
@@ -225,7 +224,7 @@ Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla s
                     xs={10}
                     sm={10}
                     md={4}
-                    lg={3}
+                    lg={2}
                     style={{textAlign:'left',overflow:"hidden"}}
                     className="align-self-center"
                 >
@@ -245,7 +244,7 @@ Phasellus quis magna tellus. Quisque vulputate vel neque quis consequat. Nulla s
                     xs={10}
                     sm={10}
                     md={4}
-                    lg={3}
+                    lg={2}
                     style={{textAlign:'left',overflow:"hidden"}}
                     className="align-self-center"
                 >
